9f02cbefef5d029ee24290aecc9e5fe814aed997
xfatkh0 <erik.fatkhutdinov@gmail.com>
2021-03-03 20:57:01 +0100

    added slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,19 +1,21 @@
-$(function(){
+$(function () {
 
 
-    var mixer = mixitup('.products__inner-box'); 
+    const mixer = mixitup('.products__inner-box');
 
-    $(".rate-star").rateYo({
-        rating: 5,
-        startWidth: "12px",
-        readOnly: true,
-    });
+
 
     $('.product-slider__inner').slick({
         dots: true,
         arrows: false,
         slidesToShow: 4,
         slidesToScroll: 4,
+    });
+
+    $(".rate-star").rateYo({
+        rating: 5,
+        startWidth: "12px",
+        readOnly: true,
     });
 
 
