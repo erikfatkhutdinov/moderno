1348403f3afc546a71e9af2526d221d7e1a8c817
xfatkh0 <erik.fatkhutdinov@gmail.com>
2021-03-03 15:59:47 +0100

    added mixer

--- app/js/main.js
+++ app/js/main.js
@@ -3,5 +3,18 @@ $(function(){
 
     var mixer = mixitup('.products__inner-box'); 
 
+    $(".rate-star").rateYo({
+        rating: 5,
+        startWidth: "12px",
+        readOnly: true,
+    });
+
+    $('.product-slider__inner').slick({
+        dots: true,
+        arrows: false,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+    });
+
 
 });
